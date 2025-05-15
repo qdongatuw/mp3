cbecc33dab7d2d8f4c7ae3a1ae3db4503dfa9006
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2025-05-15 16:58:23 -0500

    Update mp4_cont.py

--- mp4_cont.py
+++ mp4_cont.py
@@ -1,12 +1,12 @@
 from tkinter.filedialog import askopenfilenames
-# import moviepy
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+import moviepy
+# from moviepy import editor
 
 # 读取所有视频
 clips = askopenfilenames(defaultextension='.mp4')
 
 # 拼接视频
-final_clip = concatenate_videoclips(clips)
+final_clip = moviepy.concatenate_videoclips(clips)
 
 # 导出结果
 final_clip.write_videofile("output.mp4")
